7b57b6a1abaf9c4f1e88e7126ee3e7ffc1cbaf99
Jessica Corona <jessicacorona51@yahoo.com>
2020-05-21 23:03:48 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -3,7 +3,8 @@ function basicTeenager(age) {
 }
 
 function teenager(age) {
-
+ if (age >=13 && age <=19) {return "you are a teenager!"}
+ else {return "You are not a teenager"}
 }
 
 function ageChecker(age) {
